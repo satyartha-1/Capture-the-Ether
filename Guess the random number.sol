e7229f8bc5c18b569bcd8e8c8ca532ec2ca7ba74
satyartha 2.0 <88144246+satyartha-1@users.noreply.github.com>
2022-01-02 23:11:03 +0530

    Update Guess the random number.sol

--- Guess the random number.sol
+++ Guess the random number.sol
@@ -7,7 +7,20 @@ contract GuessTheRandomNumberChallenge {
         require(msg.value == 1 ether);
         answer = uint8(keccak256(block.blockhash(block.number - 1), now));
     }
+/* 
+in this challenge we need to find the block number and now value to calculate the hash. Here the block number is the one in which the current transaction is included.
+We find the block number by searching our tx hash on etherscan i.e 0x2a6c25466ede5bcb22db483ac906d473efbfdbd8c5026afe236b8bf5a146d61c.
+In our case, block details are at https://ropsten.etherscan.io/block/11720099 . So block.number - 1 equals 11720098. To find the blockhash of block 11720098 we look at hash at https://ropsten.etherscan.io/block/11720098
+OR we can refer the parent hash of block 11720099.
 
+To find 'now', look at the timestamp of our transaction i.e https://ropsten.etherscan.io/tx/0x2a6c25466ede5bcb22db483ac906d473efbfdbd8c5026afe236b8bf5a146d61c. But this time is 
+in the date time format. We need to convert it to epoch time in order for it to be used for calculating the hash. Use online converters for this purpose. Epoch time is entered 
+in seconds. 
+
+The data types of data is very important as it changes the calculated hash. _hash is a bytes32 type and epoch time is a uint256 type.
+
+Finally we convert the calculated answer2 to uint8 to match with data type of answer.
+*/
     function isComplete() public view returns (bool) {
         return address(this).balance == 0;
     }
